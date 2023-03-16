8770413d66a00b267f3e2a47a2682ca04f3472eb
TuPham2803 <tupham2832003@gmail.com>
2023-03-16 19:48:28 +0700

    first commit

---
+++ BaiNhom/LopHangSo.cs
@@ -0,0 +1,13 @@
+﻿namespace BaiNhom
+{
+    internal class LopHangSo
+    {
+        public static int chieuDaiOCo = 30;
+        public static int chieuCaoOCo = 30;
+
+        public static int chieuDaiBanCo = 30;
+        public static int chieuCaoBanCo = 15;
+
+        public static string path = @"xao\";
+    }
+}
